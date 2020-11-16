a3a9c0f88636324d69274456dc1820010f1539df
Wesley Dowling <wezzhad@gmail.com>
2020-11-17 10:15:02 +1100

    Update sketch.js

--- UX/create_capture_debug/sketch.js
+++ UX/create_capture_debug/sketch.js
@@ -9,7 +9,7 @@ function setup() {
   //the createCapture() function creates an HTML video tag
   //as well as pulls up image to be used in p5 canvas
   //hide() function hides the HTML video element
-  cam.hide();
+  //cam.hide();
   cam.elt.setAttribute('playsinline', '');
 }
 
