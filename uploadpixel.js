a2bf2871dd0e0ffaac8cf552ee3ac29088aff9a7
Liam Power <liamfpower@gmail.com>
2021-01-06 12:55:37 +1100

    merged main

--- uploadpixel.js
+++ uploadpixel.js
@@ -101,57 +101,57 @@ function shaderDraw() {
       color_lerp = color_lerp - 1;
     }
   }
-}
 
-//if (pixelShaderToggle){
-backgroundcol = colorDraw(colour);
-//  }
-let mx = map(mouseX, 0, width, 0, 1);
-let my = map(mouseY, 0, height, 0, 1);
+  //if (pixelShaderToggle){
+  backgroundcol = colorDraw(colour);
+  //  }
+  let mx = map(mouseX, 0, width, 0, 1);
+  let my = map(mouseY, 0, height, 0, 1);
 
-let pipx = width - 30;
-let pipy = height - 30; //width < 900 ? height-(height/6) : height-30;
+  let pipx = width - 30;
+  let pipy = height - 30; //width < 900 ? height-(height/6) : height-30;
 
-let pip_x = map(pipx, 0, width, 1, 0);
-let pip_y = map(pipy, 0, height, 0, 1);
+  let pip_x = map(pipx, 0, width, 1, 0);
+  let pip_y = map(pipy, 0, height, 0, 1);
 
-let pip_mx = norm(map(mouseX, 0, width, pipx - width / 5, pipx), 0, width);
-let pip_my = norm(map(mouseY, 0, height, pipy - height / 5, pipy), 0, height);
+  let pip_mx = norm(map(mouseX, 0, width, pipx - width / 5, pipx), 0, width);
+  let pip_my = norm(map(mouseY, 0, height, pipy - height / 5, pipy), 0, height);
 
-backgroundShader.setUniform("u_resolution", [width, height]);
-backgroundShader.setUniform("u_mouse", [mx, my]);
-backgroundShader.setUniform("u_pip", [pip_x, pip_y]);
-backgroundShader.setUniform("u_pip_mouse", [pip_mx, pip_my]);
-backgroundShader.setUniform("tex1", pixelpg);
-backgroundShader.setUniform("u_color", [
-  backgroundcol.levels[0],
-  backgroundcol.levels[1],
-  backgroundcol.levels[2],
-]);
-backgroundShader.setUniform("u_lerp", map(color_lerp, 0, 200, 0, 1));
-backgroundShader.setUniform("u_lerp2", map(lerp_amount, 0, 200, 0, 1));
+  backgroundShader.setUniform("u_resolution", [width, height]);
+  backgroundShader.setUniform("u_mouse", [mx, my]);
+  backgroundShader.setUniform("u_pip", [pip_x, pip_y]);
+  backgroundShader.setUniform("u_pip_mouse", [pip_mx, pip_my]);
+  backgroundShader.setUniform("tex1", pixelpg);
+  backgroundShader.setUniform("u_color", [
+    backgroundcol.levels[0],
+    backgroundcol.levels[1],
+    backgroundcol.levels[2],
+  ]);
+  backgroundShader.setUniform("u_lerp", map(color_lerp, 0, 200, 0, 1));
+  backgroundShader.setUniform("u_lerp2", map(lerp_amount, 0, 200, 0, 1));
 
-pipShader.setUniform("u_resolution", [width, height]);
-pipShader.setUniform("u_mouse", [mx, my]);
-pipShader.setUniform("u_pip", [pip_x, pip_y]);
-pipShader.setUniform("u_pip_mouse", [pip_mx, pip_my]);
-pipShader.setUniform("tex1", pixelpg);
-pipShader.setUniform("u_color", [
-  backgroundcol.levels[0],
-  backgroundcol.levels[1],
-  backgroundcol.levels[2],
-]);
-pipShader.setUniform("u_lerp", map(color_lerp, 0, 200, 0, 1));
-pipShader.setUniform("u_lerp2", map(lerp_amount, 0, 200, 0, 1));
+  pipShader.setUniform("u_resolution", [width, height]);
+  pipShader.setUniform("u_mouse", [mx, my]);
+  pipShader.setUniform("u_pip", [pip_x, pip_y]);
+  pipShader.setUniform("u_pip_mouse", [pip_mx, pip_my]);
+  pipShader.setUniform("tex1", pixelpg);
+  pipShader.setUniform("u_color", [
+    backgroundcol.levels[0],
+    backgroundcol.levels[1],
+    backgroundcol.levels[2],
+  ]);
+  pipShader.setUniform("u_lerp", map(color_lerp, 0, 200, 0, 1));
+  pipShader.setUniform("u_lerp2", map(lerp_amount, 0, 200, 0, 1));
 
-// rect gives us some geometry on the screen
-pixelpg.rect(0, 0, width, height);
-backgroundpg.rect(0, 0, width, height);
-pippg.rect(0, 0, width, height);
+  // rect gives us some geometry on the screen
+  pixelpg.rect(0, 0, width, height);
+  backgroundpg.rect(0, 0, width, height);
+  pippg.rect(0, 0, width, height);
 
-if (pixelShaderToggle) {
-  image(backgroundpg, 0, 0);
-  image(pippg, 0, 0);
+  if (pixelShaderToggle) {
+    image(backgroundpg, 0, 0);
+    image(pippg, 0, 0);
+  }
 }
 
 function removeData() {
