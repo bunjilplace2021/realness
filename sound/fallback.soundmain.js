be7983a4c538dd32882566e923014e1e7e3165f4
Liam Power <liamfpower@gmail.com>
2021-01-06 16:57:33 +1100

    merged sound to dev branch

---
+++ sound/fallback.soundmain.js
@@ -0,0 +1,26 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is not neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkmy_webpack_project"] = self["webpackChunkmy_webpack_project"] || []).push([["fallback"],{
+
+/***/ "./src/app/samples/fallback.mp3":
+/*!**************************************!*\
+  !*** ./src/app/samples/fallback.mp3 ***!
+  \**************************************/
+/*! namespace exports */
+/*! export default [provided] [no usage info] [missing usage info prevents renaming] */
+/*! other exports [not provided] [no usage info] */
+/*! runtime requirements: __webpack_exports__, __webpack_require__.r, __webpack_require__.p, __webpack_require__.d, __webpack_require__.* */
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => __WEBPACK_DEFAULT_EXPORT__\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + \"9a2f98adf5da7ee822ed2dbdcdf03159.mp3\");\n\n//# sourceURL=webpack://my-webpack-project/./src/app/samples/fallback.mp3?");
+
+/***/ })
+
+}]);
