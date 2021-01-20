a746e787e858edfe55a9485f92e0d3a92cc8f5ca
Liam <liamfpower@gmail.com>
2021-01-20 14:46:26 +1100

    notes scale to pixel data

--- firebase.js
+++ firebase.js
@@ -1,44 +1,47 @@
 function firebasesetup() {
-  var firebaseConfig = {
-    apiKey: "AIzaSyAbb8-7skMg99nzAlvUaqR6vfQvD7q_7Vs",
-    authDomain: "test-a201f.firebaseapp.com",
-    databaseURL: "https://test-a201f.firebaseio.com",
-    projectId: "test-a201f",
-    storageBucket: "test-a201f.appspot.com",
-    messagingSenderId: "120555329679",
-    appId: "1:120555329679:web:a8734f69a5da95480df8fa",
-    measurementId: "G-QLSEEEE02P",
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
+	var firebaseConfig = {
+		apiKey: 'AIzaSyAbb8-7skMg99nzAlvUaqR6vfQvD7q_7Vs',
+		authDomain: 'test-a201f.firebaseapp.com',
+		databaseURL: 'https://test-a201f.firebaseio.com',
+		projectId: 'test-a201f',
+		storageBucket: 'test-a201f.appspot.com',
+		messagingSenderId: '120555329679',
+		appId: '1:120555329679:web:a8734f69a5da95480df8fa',
+		measurementId: 'G-QLSEEEE02P'
+	};
+	// Initialize Firebase
+	firebase.initializeApp(firebaseConfig);
+	firebase.analytics();
 
-  database = firebase.database();
+	database = firebase.database();
 
-  var ref = database.ref("test3").limitToLast(array_limit);
-  ref.on("child_added", gotData, errData);
+	var ref = database.ref('test3').limitToLast(array_limit);
+	ref.on('child_added', gotData, errData);
 
-  //feedRef = firebase.database().ref().child('feed').child(user_id).limitToLast(100);
+	//feedRef = firebase.database().ref().child('feed').child(user_id).limitToLast(100);
 }
 
 function gotData(data) {
-  window.dispatchEvent(window.pixelAddEvent);
-  var test = data.val();
-  var keys = Object.keys(test);
-
-  ps.addParticle(
-    test.mouseX_loc,
-    test.mouseY_loc,
-    0,
-    test.colour_loc,
-    test.deviceWidth,
-    test.deviceHeight,
-    test.touchTime,
-    test.uuid
-  );
+	var test = data.val();
+	var keys = Object.keys(test);
+	window.pixelAddEvent.data = {
+		pixelX: test.mouseX_loc,
+		pixelY: test.mouseY_loc
+	};
+	window.dispatchEvent(window.pixelAddEvent);
+	ps.addParticle(
+		test.mouseX_loc,
+		test.mouseY_loc,
+		0,
+		test.colour_loc,
+		test.deviceWidth,
+		test.deviceHeight,
+		test.touchTime,
+		test.uuid
+	);
 }
 
 function errData(err) {
-  console.log("Error!");
-  console.log(err);
+	console.log('Error!');
+	console.log(err);
 }
