9354a429063c772d7f84029e0c6ef94a9fa19ec4
wezzahd <wesley@wesleydowling.com>
2021-02-05 09:54:35 +1100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -221,6 +221,18 @@ function mouseReleased() {
   mouseIsReleased = true;
 }
 
+function touchStarted(){
+  return false;
+}
+
+function touchMoved(){
+  return false;
+}
+
+function touchEnded(){
+  return false;
+}
+
 function keyPressed() {
   //Hide hamburger
 
