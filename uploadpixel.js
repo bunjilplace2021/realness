fea4e9cde4560ae02b6863b970d814c16fc87ed8
wezzahd <wesley@wesleydowling.com>
2021-03-06 16:50:06 +1100

    updated pixelpg.get function
    changed to pixel array lookup function.

--- uploadpixel.js
+++ uploadpixel.js
@@ -18,209 +18,210 @@ let rect;
 //load shader for camera module
 
 function shaderPreload() {
-  // load the shader
-  pixelShader = loadShader("shader/vertex.vert", "shader/aspect.frag");
-  pipShader = loadShader("shader/vertex.vert", "shader/pip.frag");
+	// load the shader
+	pixelShader = loadShader('shader/vertex.vert', 'shader/aspect.frag');
+	pipShader = loadShader('shader/vertex.vert', 'shader/pip.frag');
 }
 
 function shaderSetup() {
-  // initialize the webcam at the window size
+	// initialize the webcam at the window size
 
-  cam = createCapture(VIDEO, hasGetUserMedia());
+	cam = createCapture(VIDEO, hasGetUserMedia());
 
-  cam.elt.setAttribute("playsinline", "");
+	cam.elt.setAttribute('playsinline', '');
 
-  pixelpg = createGraphics(cnv.width, cnv.height, WEBGL);
-  pippg = createGraphics(cnv.width, cnv.height, WEBGL);
+	pixelpg = createGraphics(cnv.width, cnv.height, WEBGL);
+	pippg = createGraphics(cnv.width, cnv.height, WEBGL);
 
-  colour = color(0, 0, 0);
-  oldcolour = color(0, 0, 0);
-  backgroundcol = color(0, 0, 0);
 
-  startColor = color(255, 255, 255);
-  newColor = color(255, 255, 255);
-  amt = 0;
+	colour = color(0, 0, 0);
+	oldcolour = color(0, 0, 0);
+	backgroundcol = color(0, 0, 0);
+
+	startColor = color(255, 255, 255);
+	newColor = color(255, 255, 255);
+	amt = 0;
 }
 
 function shaderDraw() {
-  // shader() sets the active shader with our shader
-  pixelpg.shader(pixelShader);
+	// shader() sets the active shader with our shader
+	pixelpg.shader(pixelShader);
 
-  if (pixelShaderToggle) {
-    if (lerp_amount < 50) {
-      lerp_amount = lerp_amount + 1;
-    }
-  } else {
-    if (lerp_amount >= 0) {
-      lerp_amount = lerp_amount - 1;
-    }
-  }
+	if (pixelShaderToggle) {
+		if (lerp_amount < 50) {
+			lerp_amount = lerp_amount + 1;
+		}
+	} else {
+		if (lerp_amount >= 0) {
+			lerp_amount = lerp_amount - 1;
+		}
+	}
 
-  pixelShader.setUniform("tex0", cam);
-  pixelShader.setUniform("u_resolution", [width, height]);
-  pixelShader.setUniform("u_lerp", 1); //map(lerp_amount, 0, 50, 0, 1)
-  pixelShader.setUniform("u_safari", isSafari ? 1 : 0);
-  // lets just send the cam to our shader as a uniform
-  if (!isMobile) {
-    //check camera and device orientation on mobile
-    pixelShader.setUniform("u_devicecamres", [cam.width, cam.height]);
-  } else {
-    if (height > width) {
-      pixelShader.setUniform("u_devicecamres", [cam.width, cam.height]);
-    } else {
-      pixelShader.setUniform("u_devicecamres", [cam.height, cam.width]);
-    }
-  }
-  pixelpg.rect(0, 0, width, height);
+	pixelShader.setUniform('tex0', cam);
+	pixelShader.setUniform('u_resolution', [width, height]);
+	pixelShader.setUniform('u_lerp', 1); //map(lerp_amount, 0, 50, 0, 1)
+	pixelShader.setUniform('u_safari', isSafari ? 1 : 0);
+	// lets just send the cam to our shader as a uniform
+	if (!isMobile) {
+		//check camera and device orientation on mobile
+		pixelShader.setUniform('u_devicecamres', [cam.width, cam.height]);
+	} else {
+		if (height > width) {
+			pixelShader.setUniform('u_devicecamres', [cam.width, cam.height]);
+		} else {
+			pixelShader.setUniform('u_devicecamres', [cam.height, cam.width]);
+		}
+	}
+	pixelpg.rect(0, 0, width, height);
 }
 
 function pipShaderDraw() {
-  //	backgroundpg.shader(backgroundShader);
-  pippg.shader(pipShader);
+//	backgroundpg.shader(backgroundShader);
+	pippg.shader(pipShader);
 
-  let mx = map(mouseX, 0, width, 0, 1);
-  let my = map(mouseY, 0, height, 0, 1);
+	let mx = map(mouseX, 0, width, 0, 1);
+	let my = map(mouseY, 0, height, 0, 1);
 
-  let pipx = width - 30;
-  let pipy =
-    isMobile && width < height
-      ? document.documentElement.clientHeight - 30
-      : height - 30;
+	let pipx = width - 30;
+	let pipy = isMobile && width < height ? document.documentElement.clientHeight - 30 : height - 30;
 
-  let pip_x = map(pipx, 0, width, 1, 0);
-  let pip_y = map(pipy, 0, height, 0, 1);
+	let pip_x = map(pipx, 0, width, 1, 0);
+	let pip_y = map(pipy, 0, height, 0, 1);
 
-  let pip_mx = norm(map(mouseX, 0, width, pipx - width / 5, pipx), 0, width);
-  let pip_my = norm(map(mouseY, 0, height, pipy - height / 5, pipy), 0, height);
+	let pip_mx = norm(map(mouseX, 0, width, pipx - width / 5, pipx), 0, width);
+	let pip_my = norm(map(mouseY, 0, height, pipy - height / 5, pipy), 0, height);
 
-  pipShader.setUniform("u_resolution", [width, height]);
-  pipShader.setUniform("u_mouse", [mx, my]);
-  pipShader.setUniform("u_pip", [pip_x, pip_y]);
-  pipShader.setUniform("u_pip_mouse", [pip_mx, pip_my]);
-  pipShader.setUniform("tex1", pixelpg);
-  pipShader.setUniform("u_color", [
-    backgroundcol.levels[0],
-    backgroundcol.levels[1],
-    backgroundcol.levels[2],
-  ]);
+	pipShader.setUniform('u_resolution', [width, height]);
+	pipShader.setUniform('u_mouse', [mx, my]);
+	pipShader.setUniform('u_pip', [pip_x, pip_y]);
+	pipShader.setUniform('u_pip_mouse', [pip_mx, pip_my]);
+	pipShader.setUniform('tex1', pixelpg);
+	pipShader.setUniform('u_color', [backgroundcol.levels[0], backgroundcol.levels[1], backgroundcol.levels[2]]);
 
-  // rect gives us some geometry on the screen
+	// rect gives us some geometry on the screen
 
-  pippg.rect(0, 0, width, height);
+	pippg.rect(0, 0, width, height);
+
 }
 
 function removeData() {
-  var testremove = database.ref("test3");
-  testremove
-    .remove()
-    .then(function () {
-      console.log("Remove succeeded.");
-    })
-    .catch(function (error) {
-      console.log("Remove failed: " + error.message);
-    });
+	var testremove = database.ref('test3');
+	testremove
+		.remove()
+		.then(function() {
+			console.log('Remove succeeded.');
+		})
+		.catch(function(error) {
+			console.log('Remove failed: ' + error.message);
+		});
 
-  //color_lerp = 0;
+	//color_lerp = 0;
 }
 
 function hasGetUserMedia() {
-  //permission check
-  let constraints = {
-    video: true,
-    // audio: true
-  };
+	//permission check
+	let constraints = {
+		video: true
+		// audio: true
+	};
 
-  navigator.mediaDevices
-    .getUserMedia(constraints)
-    .then(function (stream) {
-      webcam_permission = true;
-      webcam = true;
-      console.log("webcam connected");
-      window.stream = stream;
-    })
-    .catch(function (err) {
-      webcam = false;
-      webcam_permission = false;
-      console.log("No Webcam", err);
-    });
+	navigator.mediaDevices
+		.getUserMedia(constraints)
+		.then(function(stream) {
+			webcam_permission = true;
+			webcam = true;
+			console.log('webcam connected');
+			window.stream = stream;
+		})
+		.catch(function(err) {
+			webcam = false;
+			webcam_permission = false;
+			console.log('No Webcam', err);
+		});
 }
 
 function webcamCheck() {
-  //check if still connected
-  navigator.mediaDevices
-    .enumerateDevices()
-    .then((devices) => {
-      const cameras = devices.filter((d) => d.kind === "videoinput");
-      if (cameras.length > 0) {
-        webcam = true;
-      } else {
-        webcam = false;
-      }
-    })
-    .catch(function (err) {
-      console.log(err.name + ": " + err.message);
-      webcam = false;
-    });
+	//check if still connected
+	navigator.mediaDevices
+		.enumerateDevices()
+		.then((devices) => {
+			const cameras = devices.filter((d) => d.kind === 'videoinput');
+			if (cameras.length > 0) {
+				webcam = true;
+			} else {
+				webcam = false;
+			}
+		})
+		.catch(function(err) {
+			console.log(err.name + ': ' + err.message);
+			webcam = false;
+		});
 }
 
-function getPixels(canvas, x, y) {
-  canvas.loadPixels();
+
+function quickGet(p, x, y){
+  p.loadPixels();
+
   let d = pixelDensity();
-  let off = ~~(x + y * width) * d * 4;
+let off = (y * width + x) * d * 4;
+let components = [
+  p.pixels[off],
+  p.pixels[off + 1],
+  p.pixels[off + 2],
+  p.pixels[off + 3]
+];
 
-  return [
-    canvas.pixels[off],
-    canvas.pixels[off + 1],
-    canvas.pixels[off + 2],
-    canvas.pixels[off + 3],
-  ];
+return components;
+
 }
+
+
+
 function shaderMousePressed() {
-  if (webcam_permission) {
-    webcamCheck();
-  }
-  console.time("pixelpg.get");
-  let x = width - mouseX;
-  let y = isSafari ? mouseY : height - mouseY;
-  colour = pixelpg.get(x, y);
+	if (webcam_permission) {
+		webcamCheck();
+	}
 
-  console.timeEnd("pixelpg.get");
+	//colour = pixelpg.get(width - mouseX, isSafari ? mouseY : height - mouseY);
 
-  let rand_gen = floor(random(0, 3));
+	colour = quickGet(pixelpg, width - mouseX, isSafari ? mouseY : height - mouseY);
 
-  // texture upside down?
-  var data = {
-    uuid: uuid,
-    mouseX_loc: mouseX,
-    mouseY_loc: mouseY,
-    rand: rand_gen,
-    colour_loc: colour,
-    deviceWidth: width,
-    deviceHeight: height,
-    touchTime: touchtime,
-  };
 
-  var test = database.ref("test3");
+	let rand_gen = floor(random(0, 3));
 
-  //do not upload pixel if location under menu element check
+	// texture upside down?
+	var data = {
+		uuid: uuid,
+		mouseX_loc: mouseX,
+		mouseY_loc: mouseY,
+		rand: rand_gen,
+		colour_loc: colour,
+		deviceWidth: width,
+		deviceHeight: height,
+		touchTime: touchtime
+	};
 
-  rect = document.getElementById("top").getBoundingClientRect();
+	var test = database.ref('test3');
 
-  if (mouseY <= rect.height) {
-    menu_loc = true;
-  } else {
-    menu_loc = false;
-  }
+	//do not upload pixel if location under menu element check
 
-  if (webcam && !menu_loc) {
-    console.time("pushing to firebase");
-    test.push(data);
-    console.log(data);
-    console.timeEnd("pushing to firebase");
-  }
+	rect = document.getElementById('top').getBoundingClientRect();
+
+
+	if (mouseY <= rect.height) {
+		menu_loc = true;
+	} else {
+		menu_loc = false;
+	}
+
+
+	if (webcam && !menu_loc) {
+		test.push(data);
+		console.log(data);
+	}
 }
 
 function shaderWindowResized(w, h) {
-  pixelpg.resizeCanvas(w, h);
-  pippg.resizeCanvas(w, h);
+	pixelpg.resizeCanvas(w, h);
+	pippg.resizeCanvas(w, h);
 }
