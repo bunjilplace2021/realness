1060763c501eb7e1c7cdb5c8d95135fc99f22e5d
wezzahd <wesley@wesleydowling.com>
2021-03-10 14:46:35 +1100

    pip repositioning on mobile

--- uploadpixel.js
+++ uploadpixel.js
@@ -91,7 +91,11 @@ function pipShaderDraw() {
   let pip_y = map(pipy, 0, height, 0, 1);
 
   let pip_mx = norm(map(mouseX, 0, width, pipx - width / 5, pipx), 0, width);
-  let pip_my = norm(map(mouseY, 0, height, pipy - height / 5, pipy), height, 0);
+  let pip_my = detecttouch
+              ?   norm(map(mouseY, 0, height, pipy, pipy + height / 5), 0, height)
+              :   norm(map(mouseY, 0, height, pipy - height / 5, pipy), 0, height);
+
+  norm(map(mouseY, 0, height, pipy - height / 5, pipy), 0, height);
 
   pipShader.setUniform("u_resolution", [width, height]);
   pipShader.setUniform("u_mouse", [mx, my]);
