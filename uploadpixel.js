d0539425c48190d928395424d4c39965c77a0c9c
wezzahd <wesley@wesleydowling.com>
2021-04-06 13:29:36 +1000

    Update uploadpixel.js

--- uploadpixel.js
+++ uploadpixel.js
@@ -131,7 +131,7 @@ function shaderMousePressed() {
   var data = {
     uuid: uuid,
     audioUUID:
-      window.recording && !window.recordingLimitReached
+      window.audioUUIDs.length > 2 && !window.recordingLimitReached
         ? window.audioUUIDs[window.audioUUIDs.length - 1]
         : "noAudio",
     mouseX_loc: mouseX,
