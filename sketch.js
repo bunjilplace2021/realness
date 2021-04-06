4b03cdca47b50b6ffab611cdf645b46951a134fd
wezzahd <wesley@wesleydowling.com>
2021-04-06 14:37:20 +1000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -177,7 +177,7 @@ function mousePressed() {
 
   //sample and upload pixel to firebase
 
-  setTimeout(shaderMousePressed(),500);
+  setTimeout(shaderMousePressed,100);
   mouseIsReleased = false;
   initload = false;
 }
