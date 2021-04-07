6e5f9e4eca38af651f5efb095d1b2e7dea020fa6
wezzahd <wesley@wesleydowling.com>
2021-04-07 17:28:13 +1000

    Update particle.js

--- particle.js
+++ particle.js
@@ -309,7 +309,7 @@ class Particle {
 
           if (Array.isArray(window.audioUUIDs)){
         for (var i = 0; i < window.audioUUIDs.length; i++) {
-          if (this.active && this.audioUUID == window.audioUUIDs[i] && this.recordcount > 0 && !pixelShaderToggle && window.audioUUIDs.length > 0) {
+          if (this.active && this.audioUUID == window.audioUUIDs[i] && !pixelShaderToggle && window.audioUUIDs.length > 0) {
             this.audioBuffer(p);
           }
         }
